5d8814577b45ab26e92a1681ff19547b4378e842
Wellington Mendes <wellmend0@gmail.com>
2024-08-21 15:32:02 -0300

    add expiration for jwt token

--- api/main.py
+++ api/main.py
@@ -35,7 +35,7 @@ app.add_middleware(
 
 class Settings(BaseModel):
     AUTHJWT_SECRET_KEY: str = DEFAULT_SECRET_KEY
-    authjwt_access_token_expires: int = 60 # seconds
+    authjwt_access_token_expires: int = 3600 # 1 hour
 
 @AuthJWT.load_config
 def get_config():
