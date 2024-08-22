36d2cec9609a6ec4c0f93b38783dc1695bc70e73
Wellington Mendes <wellmend0@gmail.com>
2024-08-22 07:53:22 -0300

    remove unused constant

--- api/app/constants.py
+++ api/app/constants.py
@@ -1,12 +1,2 @@
-import bcrypt
-
 SWAPI_STARSHIPS_BASE_URL = "https://swapi.dev/api/starships"
-
 DEFAULT_SECRET_KEY = "default_secret_key"
-
-STORED_USERS = [
-    {
-        "username": "admin",
-        "password": bcrypt.hashpw("admin".encode("utf-8"), bcrypt.gensalt()),
-    }
-]
